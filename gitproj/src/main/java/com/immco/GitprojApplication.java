8b3327d28fb7e84f3839e1af1fadb39e538e02fd
Shajee Lawrence <shajee.l@immcoinc.com>
2017-01-22 11:37:24 -0700

    First Commit

---
+++ gitproj/src/main/java/com/immco/GitprojApplication.java
@@ -0,0 +1,12 @@
+package com.immco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitprojApplication.class, args);
+	}
+}
